a9ff06150e946ce8d06337f7d22fbfbeb53fa47c
caitou <845631805@qq.com>
2017-09-05 09:02:51 +0800

    首页更改为swiper版本

--- src/pages/shouye/shouye.js
+++ src/pages/shouye/shouye.js
@@ -1,2 +1,38 @@
 require('common/styles/index.styl');
 require('./shouye.styl');
+var Swiper = require('plugins/swiper/swiper-3.4.2.min.js');
+
+var settledInfoSwiper = new Swiper('.settled-info-swiper', {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    direction: 'vertical',
+    autoplay: 3000,
+    loop: true,
+    autoplayDisableOnInteraction: false
+});
+
+var newArrivalSwiper = new Swiper('.new-arrival-swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    autoplay: 4000,
+    loop: true,
+    autoplayDisableOnInteraction: false,
+    spaceBetween : -5
+});
+
+var supplyBuyingInfoSwiper = new Swiper('.supply-buying-info-swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    autoplay: 5000,
+    loop: true,
+    autoplayDisableOnInteraction: false
+});
+
+var versionSwiper = new Swiper('.version-swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    autoplay: 6000,
+    loop: true,
+    autoplayDisableOnInteraction: false,
+    spaceBetween : -5
+});
